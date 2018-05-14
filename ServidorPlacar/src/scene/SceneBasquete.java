6bbb2f85f4a748466f5340b43128c1f1454fd195
danie <danie@DESKTOP-RIONKL1>
2018-05-14 19:54:46 -0300

    Modificando Fontes

--- ServidorPlacar/src/scene/SceneBasquete.java
+++ ServidorPlacar/src/scene/SceneBasquete.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
@@ -23,6 +24,7 @@ public class SceneBasquete extends Application {
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/view/FXMLBasquete.fxml"));
         Scene scene = new Scene(root);
+        Font.loadFont(this.getClass().getResource("/estilos/fontes/digi.ttf").toExternalForm(), 23.8);
         stage.setTitle("Basquete");
         stage.setScene(scene);
         stage.show();
