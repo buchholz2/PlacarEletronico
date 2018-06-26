26c1054502fc0a1f2ba1a9576eeb3436368f0c47
buchholz2 <danielbuchholz16@gmail.com>
2018-06-26 11:04:28 -0300

    Criando Telas Volei e Padrao

--- ControladorPlaca/src/controlador/ControlPadraoController.java
+++ ControladorPlaca/src/controlador/ControlPadraoController.java
@@ -8,6 +8,12 @@ package controlador;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -16,12 +22,187 @@ import javafx.fxml.Initializable;
  */
 public class ControlPadraoController implements Initializable {
 
+    @FXML
+    private Label jLCronometro;
+
+    @FXML
+    private Label jLPontosLocal;
+
+    @FXML
+    private Label jLPontosVisitante;
+
+    @FXML
+    private Label jLFaltasVisitante;
+
+    @FXML
+    private Label jLFaltasLocal;
+
+    @FXML
+    private Label jLNomeLocal;
+
+    @FXML
+    private Label jLNomeVisitante;
+
+    @FXML
+    private Label jLPeriodo;
+
+    @FXML
+    private Label jLAcrescimo;
+
+    @FXML
+    private Button jBAlteraNome;
+
+    @FXML
+    private Button jBMaisUmPontosL;
+
+    @FXML
+    private Button jBMenosUmPontosL;
+
+    @FXML
+    private TextField jTFAlteraNomeLocal;
+
+    @FXML
+    private TextField jTFAlteraNomeVisitante;
+
+    @FXML
+    private Button jBMaisUmPontosV;
+
+    @FXML
+    private Button jBMenosUmPontosV;
+
+    @FXML
+    private Button jBMaisFaltaL;
+
+    @FXML
+    private Button jBMenosFaltaL;
+
+    @FXML
+    private Button jBMaisFaltaV;
+
+    @FXML
+    private Button jBMenosFaltaV;
+
+    @FXML
+    private Button jBNovoQuarto;
+
+    @FXML
+    private Label jLLogger;
+
+    @FXML
+    private Button jBIniciaCrono;
+
+    @FXML
+    private Button jBReiniciaCrono;
+
+    @FXML
+    private Button jBDefineCrono;
+
+    @FXML
+    private ToggleButton jTBPausaCrono;
+
+    @FXML
+    private TextField jTFValorAcrescimo;
+
+    @FXML
+    private Button jBRestauraTudo;
+
+    @FXML
+    private ToggleButton jTBIniciaProp;
+
+    @FXML
+    private Button jBVoltar;
+
+    @FXML
+    void alteraNomes(MouseEvent event) {
+
+    }
+
+    @FXML
+    void iniciaCrono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void iniciaPropaganda(MouseEvent event) {
+
+    }
+
+    @FXML
+    void maisFaltaL(MouseEvent event) {
+
+    }
+
+    @FXML
+    void maisFaltaV(MouseEvent event) {
+
+    }
+
+    @FXML
+    void maisUmL(MouseEvent event) {
+
+    }
+
+    @FXML
+    void maisUmV(MouseEvent event) {
+
+    }
+
+    @FXML
+    void menosFaltaL(MouseEvent event) {
+
+    }
+
+    @FXML
+    void menosFaltaV(MouseEvent event) {
+
+    }
+
+    @FXML
+    void menosUmL(MouseEvent event) {
+
+    }
+
+    @FXML
+    void menosUmV(MouseEvent event) {
+
+    }
+
+    @FXML
+    void novoPeriodo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void pausaCrono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void reiniciaCrono(MouseEvent event) {
+
+    }
+
+    @FXML
+    void restauraTudo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void somaArescimo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void voltarEscolhaModalidade(MouseEvent event) {
+
+    }
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
